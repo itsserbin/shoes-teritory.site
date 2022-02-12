3825b6f1b80098715c1906f352bfa22aa026021b
itsserbin <serbin.official@gmail.com>
2022-02-12 20:18:20 +0200

    update

--- resources/js/public/components/Header.vue
+++ resources/js/public/components/Header.vue
@@ -29,7 +29,7 @@
                     </li>
                     <li>
                         <a :href="exchangePolicyRoute" class="menu__link text-decoration-none">
-                            Обмен и возврат
+                            Условия возврата и обмена
                         </a>
                     </li>
                     <li>
