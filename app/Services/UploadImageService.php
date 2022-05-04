cfa417f7d6c4489ac050af9ff498c0ddde1adcc6
itsserbin <serbin.official@gmail.com>
2022-05-05 00:11:56 +0300

    up

--- app/Services/UploadImageService.php
+++ app/Services/UploadImageService.php
@@ -13,16 +13,18 @@ use Intervention\Image\Facades\Image;
 class UploadImageService
 {
 
-    /**
-     * Загрузчик превью товара.
-     *
-     * @param $data
-     * @return string
-     */
     public function uploadCategoryPreview($data)
     {
         $preview = $data['preview'];
         $filename = $preview->getClientOriginalName();
+
+        $root = $_SERVER["DOCUMENT_ROOT"];
+
+        $dir1 = $root . '/storage/category/';
+
+        if (!file_exists($dir1)) {
+            mkdir($dir1, 0755, true);
+        }
 
         Image::make($preview)->resize(350, null, function ($constraint) {
             $constraint->aspectRatio();
@@ -36,6 +38,14 @@ class UploadImageService
         $preview = $data['banner'];
         $filename = $preview->getClientOriginalName();
 
+        $root = $_SERVER["DOCUMENT_ROOT"];
+
+        $dir1 = $root . '/storage/banners/';
+
+        if (!file_exists($dir1)) {
+            mkdir($dir1, 0755, true);
+        }
+
         Image::make($preview)->save(public_path('storage/banners/' . $filename));
 
         return asset('/storage/banners/' . $filename);
@@ -46,19 +56,46 @@ class UploadImageService
         $image = $data['image'];
         $filename = $image->getClientOriginalName();
 
+        $root = $_SERVER["DOCUMENT_ROOT"];
+
+        $dir = $root . '/storage/products/55/';
+
+        if (!file_exists($dir)) {
+            mkdir($dir, 0755, true);
+        }
+
+        Image::make($image)->save(Storage::path('storage/products/' . $filename));
+
+
+        $dir1 = $root . '/storage/products/55/';
+
+        if (!file_exists($dir1)) {
+            mkdir($dir1, 0755, true);
+        }
+
         Image::make($image)->resize(55, null, function ($constraint) {
             $constraint->aspectRatio();
         })->save(public_path('storage/products/55/' . $filename));
+
+        $dir2 = $root . '/storage/products/350/';
+
+        if (!file_exists($dir2)) {
+            mkdir($dir2, 0755, true);
+        }
 
         Image::make($image)->resize(350, null, function ($constraint) {
             $constraint->aspectRatio();
         })->save(public_path('storage/products/350/' . $filename));
 
+        $dir3 = $root . '/storage/products/500/';
+
+        if (!file_exists($dir3)) {
+            mkdir($dir3, 0755, true);
+        }
+
         Image::make($image)->resize(500, null, function ($constraint) {
             $constraint->aspectRatio();
         })->save(public_path('storage/products/500/' . $filename));
-
-        Image::make($image)->save(public_path('storage/products/' . $filename));
 
         return $filename;
     }
